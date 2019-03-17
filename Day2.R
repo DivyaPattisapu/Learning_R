97016e60900dcd0471992106f9a7b7b7dacd6880
Uzumaki <divyapattisapu@gmail.com>
2019-03-17 10:25:27 +0530

    day2

---
+++ Day2.R
@@ -0,0 +1,11 @@
+n<-1000
+(rollno <- 1:n)
+data.frame(rollno)
+(sname=paste('Student',1:n,Sep=''))
+(gender=sample(x=c('M','F'),1000,replace=TRUE))
+(age=runif(n,min=20,max=30)
+  age <- round(age,0)
+  age
+(marks1=rnorm(n,mean=65,sd=10))
+  (marks1=rnorm(n,mean=70,sd=8))
+  
